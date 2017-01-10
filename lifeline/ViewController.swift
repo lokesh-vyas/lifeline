5e3d7632eb82ede83ec36ff1d96dcd1f3e169b63
PankajKumarNigam <pankaj.n@isteer.in>
2017-01-10 11:39:09 +0530

    Second Commit/ButtonLabelTextfield

--- lifeline/ViewController.swift
+++ lifeline/ViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblWelcome: UILabel!
+    @IBOutlet weak var txtField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func btnOKTapped(_ sender: Any) {
+        
+        lblWelcome.text = txtField.text
     }
 
 
