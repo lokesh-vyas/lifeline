d06fb17d068dc566168c017d79eb2baded225b7d
iSteer <isteer@iSteers-MacBook.local>
2017-01-11 18:44:57 +0530

    11/01/2017 FB and G+ Login by Lokesh

--- lifeline/Helper/HudBar.swift
+++ lifeline/Helper/HudBar.swift
@@ -9,10 +9,64 @@
 import Foundation
 import APESuperHUD
 
+
 class HudBar
 {
-    func showHudWithTitleAndMessage(message:String,view:UIView)
+    //SingleTon Object
+    static let sharedInstance : HudBar =
     {
-        APESuperHUD.showOrUpdateHUD(loadingIndicator: .standard, message: "Facebook login", presentingView: view)
+        let instance = HudBar()
+        return instance
+    }()
+    
+    func showHudWithMessage(message:String,view:UIView)
+    {
+        APESuperHUD.showOrUpdateHUD(loadingIndicator: .standard, message: message, presentingView: view)
+    }
+    func showHudWithCustomImagesAndMessage(message:String,imageName:String,view:UIView)
+    {
+    
+        APESuperHUD.showOrUpdateHUD(icon: UIImage(named: imageName)!, message: message, duration: 3.0, presentingView: view, completion:
+        { _ in
+            
+        })
+    }
+    func showHudWithLifeLineIconAndMessage(message:String,view:UIView)
+    {
+        APESuperHUD.showOrUpdateHUD(icon: UIImage(named: "LifeLine_icon.png")!, message: message, duration: 3.0, presentingView: view, completion:
+            { _ in
+                
+        })
+    }
+    func showHudWithTitleAndMessage(title:String,message:String,view:UIView)
+    {
+        APESuperHUD.showOrUpdateHUD(title: title, message: message, presentingView: view)
+        { _ in
+        }
+    }
+    func hideHudFormView(view:UIView)
+    {
+         
+        APESuperHUD.removeHUD(animated: true, presentingView: view, completion:
+        { _ in
+        })
+    }
+    func customUI()
+    {
+        APESuperHUD.appearance.cornerRadius = 12
+        APESuperHUD.appearance.animateInTime = 1.0
+        APESuperHUD.appearance.animateOutTime = 1.0
+        APESuperHUD.appearance.backgroundBlurEffect = .none
+        APESuperHUD.appearance.iconColor = UIColor.green
+        APESuperHUD.appearance.textColor =  UIColor.green
+        APESuperHUD.appearance.loadingActivityIndicatorColor = UIColor.green
+        APESuperHUD.appearance.defaultDurationTime = 4.0
+        APESuperHUD.appearance.cancelableOnTouch = true
+        APESuperHUD.appearance.iconWidth = 48
+        APESuperHUD.appearance.iconHeight = 48
+        APESuperHUD.appearance.messageFontName = "Caviar Dreams"
+        APESuperHUD.appearance.titleFontName = "Caviar Dreams"
+        APESuperHUD.appearance.titleFontSize = 22
+        APESuperHUD.appearance.messageFontSize = 14
     }
 }
---
+++ lifeline/Helper/StringList.swift
@@ -0,0 +1,18 @@
+//
+//  StringList.swift
+//  lifeline
+//
+//  Created by iSteer on 11/01/17.
+//  Copyright © 2017 iSteer. All rights reserved.
+//
+
+import Foundation
+
+public enum StringList: String
+{
+    case GoogleClientID = "523732833608-82fkrlpmjpiupj9tv225ku5k5d86p5gf.apps.googleusercontent.com"
+    case LifeLine_User_Name = "LifeLine_User_Name"
+    case LifeLine_User_Email = "LifeLine_User_Email"
+    case LifeLine_User_ID = "LifeLine_User_ID"
+    case LL_Welcome_Message = "Welcome to LifeLine"
+}
