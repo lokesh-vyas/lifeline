10e12e661dd9cd14df0a26a8d605e20dec571072
iSteer <isteer@iSteers-MacBook.local>
2017-02-14 13:08:29 +0530

    Request View Outlet

--- lifeline/ViewController/RequestView.swift
+++ lifeline/ViewController/RequestView.swift
@@ -11,17 +11,63 @@ import UIKit
 class RequestView: UIViewController
 {
     //MARK:- IBOutlet
-
+    @IBOutlet weak var txtFieldPatientName: FloatLabelTextField!
+    @IBOutlet weak var txtFieldContactPerson: FloatLabelTextField!
+    @IBOutlet weak var txtFieldContactNumber: FloatLabelTextField!
+    @IBOutlet weak var txtFieldHospitalBloodBankName: FloatLabelTextField!
+    @IBOutlet weak var txtFieldDoctorName: FloatLabelTextField!
+    @IBOutlet weak var txtFieldHospitalBloodBankContactNumber: FloatLabelTextField!
+    @IBOutlet weak var txtFieldHospitalBloodBankAddress: FloatLabelTextField!
+    @IBOutlet weak var txtFieldHospitalBloodBankAddressLandMark: FloatLabelTextField!
+    @IBOutlet weak var txtFieldHospitalBloodBankAddressCity: FloatLabelTextField!
+    @IBOutlet weak var txtFieldHospitalBloodBankAddressPINCode: FloatLabelTextField!
+    @IBOutlet weak var txtViewPersonalAppeal: UITextView!
+    
+    @IBOutlet weak var btnBloodUnit: UIButton!
+    @IBOutlet weak var btnBloodGroup: UIButton!
+    @IBOutlet weak var btnWhenYouNeed: UIButton!
+    @IBOutlet weak var btnWhatYouNeed: UIButton!
+    
+    @IBOutlet weak var imgSocialShare: UIImageView!
+    
     //MARK:- viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
         // Do any additional setup after loading the view.
     }
-   //MARK:- Back Button
-    @IBAction func BackButton(_ sender: Any)
+    //MARK:- btnWhatYouNeedTapped
+    @IBAction func btnWhatYouNeedTapped(_ sender: Any)
+    {
+    }
+    //MARK:- btnWhenYouNeedTapped
+    @IBAction func btnWhenYouNeedTapped(_ sender: Any)
+    {
+    }
+    //MARK:- btnBloodGroupTapped
+    @IBAction func btnBloodGroupTapped(_ sender: Any)
+    {
+    }
+    //MARK:- btnBloodUnitTapped
+    @IBAction func btnBloodUnitTapped(_ sender: Any)
+    {
+    }
+    //MARK:- GoogleMap IBAction
+    @IBAction func btnGoogleMapTapped(_ sender: Any)
+    {
+    }
+    //MARK:- SwitchShareAction
+    @IBAction func switchShareTapped(_ sender: Any)
+    {
+    }
+    //MARK:- btnBackTapped
+    @IBAction func btnBackTapped(_ sender: Any)
     {
         let SWRevealView = self.storyboard!.instantiateViewController(withIdentifier: "SWRevealViewController") as! SWRevealViewController
         self.navigationController?.present(SWRevealView, animated: true, completion: nil)
+    }
+    //MARK:- btnSubmmitTapped
+    @IBAction func btnSubmitTapped(_ sender: Any)
+    {
     }
 }
