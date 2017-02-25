8a02791bbf6be0c3e9b087bed06dc5c6cb840aee
Lokesh <lokesh.vyas@isteer.in>
2017-02-25 15:22:28 +0530

    Local changes

---
+++ lifeline/Cell/MyRequestCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyRequestCell.swift
+//  lifeline
+//
+//  Created by iSteer on 25/02/17.
+//  Copyright © 2017 iSteer. All rights reserved.
+//
+
+import UIKit
+
+class MyRequestCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
