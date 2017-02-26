09ad5ea494ef0ab198e7b4e480fd2391f2514c38
Lokesh <lokesh.vyas@isteer.in>
2017-02-26 10:58:23 +0530

    My Request View Cell

--- lifeline/Cell/MyRequestCell.swift
+++ lifeline/Cell/MyRequestCell.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class MyRequestCell: UITableViewCell {
 
+    @IBOutlet weak var lblPatientName: UILabel!
+    @IBOutlet weak var lblBloodGroup: UILabel!
+    @IBOutlet weak var lblRequestDate: UILabel!
+    @IBOutlet weak var btnCloseRequest: UIButton!
+    @IBOutlet weak var btnViewDonars: UIButton!
+    @IBOutlet weak var viewColorForStatus: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- lifeline/ViewController/MyRequestView.swift
+++ lifeline/ViewController/MyRequestView.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class MyRequestView: UIViewController {
 
+    @IBOutlet weak var tableRequestView: UITableView!
     //MARK:- viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.completelyTransparentBar()
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        tableRequestView.contentInset = UIEdgeInsetsMake(-35, 0.0, -20, 0.0)
         // Do any additional setup after loading the view.
     }
     //MARK:- BackButton
@@ -22,5 +24,21 @@ class MyRequestView: UIViewController {
     {
         let SWRevealView = self.storyboard!.instantiateViewController(withIdentifier: "SWRevealViewController") as! SWRevealViewController
         self.navigationController?.present(SWRevealView, animated: true, completion: nil)
+    }
+}
+extension MyRequestView:UITableViewDelegate,UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        var cell:MyRequestCell? = tableRequestView.dequeueReusableCell(withIdentifier: "MyRequestCell") as? MyRequestCell
+        if cell == nil
+        {
+            let nib:Array = Bundle.main.loadNibNamed("MyRequestCell", owner: self, options: nil)!
+            cell = nib[0] as? MyRequestCell
+        }
+        return cell!
     }
 }
