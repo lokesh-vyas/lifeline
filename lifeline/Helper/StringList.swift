5ca0fc0d0c4f48bb8733c31c879a1debfa14ef21
Anjali <anjali@Anjalis-MacBook-Pro.local>
2017-05-26 13:25:29 +0530

    first commit for i18n

--- lifeline/Helper/StringList.swift
+++ lifeline/Helper/StringList.swift
@@ -103,16 +103,3 @@ public enum URLList:String
     
     
 }
-//MARK:- STRING LIST
-public enum StringList: String
-{
-    
-    case GoogleClientID = "523732833608-82fkrlpmjpiupj9tv225ku5k5d86p5gf.apps.googleusercontent.com"
-    case LifeLine_User_Name = "LifeLine_User_Name"
-    case LifeLine_User_Email = "LifeLine_User_Email"
-    case LifeLine_User_ID = "LifeLine_User_Unique_ID"
-    case LL_Welcome_Message = "Welcome to LifeLine"
-    case LifeLine_HostName = "api.lifeline.services"
-    case LifeLine_Internet_Error_Message = "Unable to access service, Please try again leter."
-    case LifeLine_BloodDonation_Share_Text = "A blood donation request has been created on LifeLine.Please volunteer to donate blood or share this within your circle. Here are the details:"
-}
